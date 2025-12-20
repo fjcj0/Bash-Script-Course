d0f0a1f65a85d29acc7d03b129f93dc5395d42ab
omar <omarqais225@gmail.com>
2025-12-20 15:21:35 +0200

    less than and greate than methods

--- bash.sh
+++ bash.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 number=300
-if [ $number -ne 200 ]
+if [ $number -gt 200 ]
+then
+echo "The condition is true"
+else
+echo "The condition is false"
+fi
+if [ $number -lt 200 ]
 then
 echo "The condition is true"
 else
