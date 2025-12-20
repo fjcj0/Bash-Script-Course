efdc7fc60778e5a875ad0dabb701354cf0676a9f
omar <omarqais225@gmail.com>
2025-12-20 15:16:03 +0200

    if statment

--- bash.sh
+++ bash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sum=$((10 + 20))
-echo $sum
-let new_sum=sum+50
-echo $new_sum  
-let final_sum=$((sum+100))
-echo $final_sum
+number=200
+if [ $number -eq 200 ]
+then
+echo "The condition is true"
+fi
