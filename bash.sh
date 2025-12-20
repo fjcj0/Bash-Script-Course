d6f984eeed2caf2306591d088e72bf6b3421cb1d
omar <omarqais225@gmail.com>
2025-12-20 16:12:32 +0200

    or and operator && nested if

--- bash.sh
+++ bash.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-if [ -f ./bash.sh ]
+let number=400
+if [ $number -gt 200 ];
+then
+if [ "$number" -gt 200 ] && [ "$number" -lt 500 ]; 
 then 
-echo "This file exist"
-else 
-echo "This file not exist"
+echo "The condition is true"
+else
+echo "The condition is false"
+fi
+fi
+
+if [ "$number" -gt 200 ] || [ "$number" -eq 300 ]; then
+    echo "One of condition is true"
+else
+    echo "none of them is true"
+fi
+
+if [ "$number" -gt 500 ] || [ "$number" -eq 300 ]; then
+    echo "One of condition is true"
+else
+    echo "none of them is true"
 fi
