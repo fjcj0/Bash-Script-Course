0eecaaf44e878f1d6dd5014ca2f31f8b3f8cd842
omar <omarqais225@gmail.com>
2025-12-21 00:02:44 +0200

    while loop

--- bash.sh
+++ bash.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-a=1
-b=0
-if (( a ^ b )); then
-    echo "XOR is true"
-else
-    echo "XOR is false"
-fi
-echo $?
+i=0
+while [ $i -ne 11 ]
+do
+echo $i
+i=$(($i+1))
+sleep 0.5
+done
+while [ -d test ]
+do
+ echo "The test file exists"
+ sleep 0.5
+done
+echo "The file no longer exist"
