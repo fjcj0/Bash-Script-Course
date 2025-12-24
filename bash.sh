48f898580f1d85e1bde5ea607c03ff097322d074
omar <omarqais225@gmail.com>
2025-12-24 19:03:34 +0200

    return function bash script

--- bash.sh
+++ bash.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 add_two_num() {
-  local sum=$(($1 + $2)) 
-  echo "Sum of $1 and $2 is $sum"
+  local sum=$(($1 + $2))
+  if [ "$sum" -eq 0 ]; then
+    return 0   
+  else
+    return 1  
+  fi
 }
 add_two_num 2 3
+if [ $? -eq 0 ]; then
+  echo "The result is 0"
+else
+  echo "The result is not 0"
+fi
+result=$(add_two_num 2 3)
+if [ "$result" -eq 0 ]; then
+  echo "The result is 0"
+else
+  echo "The result is not 0"
+fi
